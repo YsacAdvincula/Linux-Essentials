7249af800e1ed5c657c70a5bf24191a9ba4336de
YsacAdvincula <152839626+YsacAdvincula@users.noreply.github.com>
2025-04-25 05:58:53 +0000

    Initial Commit

---
+++ Activity1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "ENTER NAME:"
+read NAME
+echo "ENTER AGE:"
+read AGE
+echo "ENTER PROGRAM:"
+read PROGRAM
+
+echo "Hello $NAME, This is your age $AGE & your program is $PROGRAM"
